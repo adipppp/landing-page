69d0e138381e8e00fcfdea63e8ccbcbe316ba16b
adipppp <adhif2004@gmail.com>
2023-06-09 14:08:46 +0700

    Add button listeners

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("sign-up-button-1").onclick = () =>
+  console.log("Button 1 clicked!");
+document.getElementById("sign-up-button-2").onclick = () =>
+  console.log("Button 2 clicked!");
